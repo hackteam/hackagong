0cfb65c8e8d547fab5c4904a0931a520481ecfb3
Ashwin Ramesh <ashwinramesh92@hotmail.com>
2013-07-21 12:04:46 +1000

    profile page fix, footer fix

--- controllers/public.py
+++ controllers/public.py
@@ -35,7 +35,7 @@ def todolists():
 @get('/logout')
 def logout():
     delete_web_session()
-    return "Logged Out"
+    redirect("")
 
 
 
@@ -45,8 +45,6 @@ def doLogin():
     '''Login'''
 
     post = request.POST.decode()
-
-    form = forms.LoginForm(post)
     dbs = db_session(close=True)
 
     username = post['username'].strip()
@@ -58,21 +56,18 @@ def doLogin():
         ws = web_session()
         ws['username'] = username
         ws['user_id'] = user.id
+        ws['profile_picture'] = user.picture
         return "success"
         #return json.dumps({"output" : "success"})
 
 
     return "fail"
-    #return json.dumps({
-    #   'form':form,
-    #   'message':'Username or password incorrect'
-    #})
 
 
 
 @post('/register',template='register.html')
 def register_post():
-
+    ws = web_session()
     post = request.POST.decode()
 
     form = forms.RegisterForm(post)
@@ -83,7 +78,6 @@ def register_post():
     #Check if username exists
     username = post['username'].strip()
     user = dbs.query(User).filter(User.username == username).first()
-
 
     if user:
         return {
@@ -111,13 +105,14 @@ def register_post():
 
 
     else:
+        ws['profile_picture'] = user.picture
         if login(post['username'].strip(), post['password']) == 'success':
             return "success"
         else:
             return "fail"
 
 def login(username, password):
-    
+
     dbs = db_session(close=True)
     user = dbs.query(User).filter(User.username == username).first()
 
--- controllers/user_stuff.py
+++ controllers/user_stuff.py
@@ -11,116 +11,122 @@ import os
 import json
 import cgi
 
-@get('/addtask/<list_id>',template="addtask.html")
+@get('/lists/<list_id>',template="addtask.html")
 @logged_in_only
 def addTask(list_id):
 
-	ws = existing_web_session()
-	ws['user_id'] = 1
-	dbs = db_session(close=True)
-	attrs = {}
+    ws = existing_web_session()
+    ws['user_id'] = 1
+    dbs = db_session(close=True)
+    attrs = {}
 
 
-	tasks = dbs.query(Task).filter(Task.user_created_id == ws['user_id'], Task.todo_list_id == list_id).all()
-	if tasks:
-		for count,task in enumerate(tasks):
-			attrs[count] = {'task_id':task.id}
-			task.date_created = task.date_created.strftime("%Y-%m-%d %H:%M:%S")
+    tasks = dbs.query(Task).filter(Task.user_created_id == ws['user_id'], Task.todo_list_id == list_id).all()
+    if tasks:
+        for count,task in enumerate(tasks):
+            attrs[count] = {'task_id':task.id}
+            task.date_created = task.date_created.strftime("%Y-%m-%d %H:%M:%S")
 
 
-	form = forms.AddTask()
+    form = forms.AddTask()
 
-	return {
-		'list_id':list_id,
-		'tasks':tasks,
-		'attrs':attrs,
-		'form':form,
-		'ws':ws
-	}
+    return {
+        'list_id':list_id,
+        'tasks':tasks,
+        'attrs':attrs,
+        'form':form,
+        'ws':ws
+    }
 
 @post('/addtask/<list_id>',template='addtask.html')
 @logged_in_only
 def add_task(list_id):
-	'''Add task for the current user'''
+    '''Add task for the current user'''
 
-	ws = existing_web_session()
-	dbs = db_session(close=True)
-	form = forms.AddTask()
-	post = request.POST.decode()
+    ws = existing_web_session()
+    dbs = db_session(close=True)
+    form = forms.AddTask()
+    post = request.POST.decode()
 
-	task_name = cgi.escape(post['task'])
+    task_name = cgi.escape(post['task'])
 
-	task = Task(name=task_name,description='',todo_list_id=list_id, creator=ws['user_id'],reviewer=ws['user_id'])
+    task = Task(name=task_name,description='',todo_list_id=list_id, creator=ws['user_id'],reviewer=ws['user_id'])
 
 
-	dbs.add(task)
+    dbs.add(task)
 
-	try:
-		dbs.commit()
-	except:
-		dbs.rollback()
-		return "-1"
+    try:
+        dbs.commit()
+    except:
+        dbs.rollback()
+        return "-1"
 
-	#tasks = dbs.query(Task).filter(Task.user_created_id == ws['user_id']).all()
+    #tasks = dbs.query(Task).filter(Task.user_created_id == ws['user_id']).all()
 
-	return json.dumps(task.get_details())
+    return json.dumps(task.get_details())
 
 @get('/upload',template='upload.html')
 def upload():
-	ws = existing_web_session()
+    ws = existing_web_session()
 
 
-	form = forms.UploadForm()
+    form = forms.UploadForm()
 
-	return {
-		'ws':ws,
-		'form':form
-	}
+    return {
+        'ws':ws,
+        'form':form
+    }
 
 @post('/upload',template='upload.html')
 def upload_video():
-	post = request.POST.decode()
-	ws = existing_web_session()
-	form = forms.UploadForm()
+    post = request.POST.decode()
+    ws = existing_web_session()
+    form = forms.UploadForm()
 
-	new_file = post['uploaded_file']
+    new_file = post['uploaded_file']
 
-	name,ext = os.path.splitext(new_file.filename)
-
-
-	#Check for upload pressed with no file selected.
-	if not new_file:
-		return {
-		    "message":"No file was selected. Please try again.",
-		    "ws":ws,
-		    'form':form
-		}
+    name,ext = os.path.splitext(new_file.filename)
 
 
-
-	name = str(uuid.uuid4())
-
-
-	OUTPUT_PATH = os.path.join(config.BASE_DIR,'vids')
-
-	if not os.path.exists(OUTPUT_PATH):
-		os.makedirs(OUTPUT_PATH)
-
-	chunk_size = 68000
-	raw = ''
-	if new_file.file:
-		while True:
-			chunk = new_file.file.read(chunk_size)
-			if not chunk:
-				break
-			raw+=chunk
+    #Check for upload pressed with no file selected.
+    if not new_file:
+        return {
+            "message":"No file was selected. Please try again.",
+            "ws":ws,
+            'form':form
+        }
 
 
-	with open(OUTPUT_PATH+'/'+name+ext, 'w') as f:
-		f.write(raw)
 
-	return {
-    	'message':'File saved successfully',
-    	'ws':ws,
-    	'form':form
+    name = str(uuid.uuid4())
+
+
+    OUTPUT_PATH = os.path.join(config.BASE_DIR,'vids')
+
+    if not os.path.exists(OUTPUT_PATH):
+        os.makedirs(OUTPUT_PATH)
+
+    chunk_size = 68000
+    raw = ''
+    if new_file.file:
+        while True:
+            chunk = new_file.file.read(chunk_size)
+            if not chunk:
+                break
+            raw+=chunk
+
+
+    with open(OUTPUT_PATH+'/'+name+ext, 'w') as f:
+        f.write(raw)
+
+    return {
+        'message':'File saved successfully',
+        'ws':ws,
+        'form':form
     }
+
+@get('/profile',template="profile.html")
+@logged_in_only
+def profile():
+    ws = existing_web_session()
+    return {'ws':ws, 'num_lists':5, 'num_tasks': 15, 'num_perks': 2}
